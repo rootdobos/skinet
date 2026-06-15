b70cf2aded6e4ed6ce1d73308066d51da224c6cc
rootdobos <49080519+rootdobos@users.noreply.github.com>
2026-06-15 13:22:37 +0300

    end of section 11

---
+++ client/src/app/core/services/busy.service.ts
@@ -0,0 +1,18 @@
+import { Service, signal } from '@angular/core';
+
+@Service()
+export class BusyService {
+  loading= signal(false);
+  busyRequestCount = 0;
+  busy() {
+    this.busyRequestCount++;
+    this.loading.set(true);
+  }
+  idle() {
+    this.busyRequestCount--;
+    if (this.busyRequestCount <= 0) {
+      this.busyRequestCount = 0;
+      this.loading.set(false);
+    }
+  }
+}
---
+++ client/src/app/core/services/snackbar.service.ts
@@ -0,0 +1,20 @@
+import { inject, Service } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Service()
+export class SnackbarService {
+  private snackbar = inject(MatSnackBar);
+
+  error(message: string) {
+    this.snackbar.open(message, 'Close', {
+      duration: 5000,
+      panelClass: ['snack-error'],
+    });
+  }
+  success(message: string) {
+    this.snackbar.open(message, 'Close', {
+      duration: 5000,
+      panelClass: ['snack-success'],
+    });
+  }
+}
