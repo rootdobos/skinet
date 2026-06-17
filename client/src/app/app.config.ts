291a7fd22a42b647132406ce1a53fddf5d9e99c2
rootdobos <49080519+rootdobos@users.noreply.github.com>
2026-06-17 12:57:24 +0300

    end of section 13

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 //import 'zone.js';
 
@@ -6,11 +13,22 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error-interceptor';
 import { loadingInterceptor } from './core/interceptors/loading-interceptor';
+import { InitService } from './core/services/init.service';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+      return lastValueFrom(initService.init()).finally(() => {
+        const splash = document.getElementById('initial-splash');
+        if (splash) {
+          splash.remove();
+        }
+      });
+    }),
+  ],
 };
--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -8,16 +8,31 @@ import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatLabel } from '@angular/material/select';
 import { MatInput } from '@angular/material/input';
 import { MatDivider } from '@angular/material/divider';
+import { CartService } from '../../../core/services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
-  imports: [CurrencyPipe, MatButton, MatIcon, MatFormField, MatInput, MatLabel, MatDivider],
+  imports: [
+    CurrencyPipe,
+    MatButton,
+    MatIcon,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatDivider,
+    FormsModule,
+  ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent implements OnInit {
   private shopService = inject(ShopService);
   private activatedRoute = inject(ActivatedRoute);
+  private cartService = inject(CartService);
+  quantityInCart = 0;
+  quantity = 1;
+
   product = signal<Product | null>(null);
   ngOnInit(): void {
     this.loadProduct();
@@ -26,8 +41,32 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (!id) return;
     this.shopService.getProduct(+id).subscribe({
-      next: (product) => this.product.set(product),
+      next: (product) => {
+        this.product.set(product);
+        this.updateQuantityInCart();
+      },
       error: (error) => console.log(error),
     });
+  }
+  updateCart() {
+    if (!this.product()) return;
+    if (this.quantity > this.quantityInCart){
+      const itemsToAdd = this.quantity - this.quantityInCart
+      this.quantityInCart +=itemsToAdd;
+      this.cartService.addItemToCart(this.product()!, itemsToAdd);
+    } else{
+      const itemsToremove = this.quantityInCart -this.quantity;
+      this.quantityInCart -= itemsToremove;
+      this.cartService.removeItemFromCart(this.product()!.id, itemsToremove)
+    }
+
+  }
+  updateQuantityInCart() {
+    this.quantityInCart =
+      this.cartService.cart()?.items.find((x) => x.productId === this.product()?.id)?.quantity || 0;
+    this.quantity = this.quantityInCart || 1;
+  }
+  getButtonText() {
+    return this.quantityInCart > 0 ? 'Update cart' : 'Add to cart';
   }
 }
