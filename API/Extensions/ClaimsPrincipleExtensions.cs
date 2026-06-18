73430ddf1c57d0fbbc9a8411f5c500c506a3c3ca
rootdobos <49080519+rootdobos@users.noreply.github.com>
2026-06-18 12:02:21 +0300

    end of section 14

---
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Security.Authentication;
+using System.Security.Claims;
+using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ClaimsPrincipleExtensions
+{
+    public static async Task<AppUser> GetUserByEmail(this UserManager<AppUser> userManager, ClaimsPrincipal user)
+    {
+        var userToReturn = await userManager.Users.FirstOrDefaultAsync(x => x.Email == user.GetEmail());
+
+        if (userToReturn != null) return userToReturn;
+        throw new AuthenticationException("User Not Found");
+    }
+    public static string GetEmail(this ClaimsPrincipal user)
+    {
+        var email = user.FindFirstValue(ClaimTypes.Email) ?? throw new AuthenticationException("Email claim not found");
+        return email;
+    }
+        public static async Task<AppUser> GetUserByEmailWithAddress(this UserManager<AppUser> userManager, ClaimsPrincipal user)
+    {
+        var userToReturn = await userManager.Users
+        .Include(x => x.Address)
+            .FirstOrDefaultAsync(x => x.Email == user.GetEmail());
+
+        if (userToReturn != null) return userToReturn;
+        throw new AuthenticationException("User Not Found");
+    }
+}
